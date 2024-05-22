29ec1f4bea05dcdd5405e04cf0eb09dced78553f
CodeDigiLab <159695671+CodeDigiLab@users.noreply.github.com>
2024-05-22 23:00:28 +0530

    track

--- include/head.js
+++ include/head.js
@@ -35,6 +35,15 @@ document.write(`
     src="https://www.facebook.com/tr?id=424966466998323&ev=PageView&noscript=1"
     /></noscript>
     <!-- End Meta Pixel Code -->
+
+    <script type='text/javascript'>
+    window.smartlook||(function(d) {
+        var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+        var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+        c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+        })(document);
+        smartlook('init', '739806ffcc7527f37c35fef251f86a454e5f84d8', { region: 'eu' });
+    </script>
     
 </head>
 
